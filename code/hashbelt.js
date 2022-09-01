dfae5348089eb3eece8c63ba1c88bb401659dfe2
Shane Holloway <shane.holloway@ieee.org>
2022-08-31 19:29:55 -0600

    Refactored to use Object.assign to enable direct extension with additional methods

--- code/hashbelt.js
+++ code/hashbelt.js
@@ -1,21 +1,22 @@
 export function hashbelt(opt={}) {
   if (opt.call) opt = {step: opt} // use function as step()
 
-  let belt = [], rotate = () => {
-    while (belt.max <= belt.length)
-      belt.expire( belt.pop() )
+  let belt = Object.assign([],
+    { interval: 60000,
+      max: 4,
+      step: Object,
+      expire: Boolean,
+      rotate() {
+        belt.unshift( belt.step() )
+        while (belt.max < belt.length)
+          belt.expire( belt.pop() )
+        return belt
+      }
+    }, opt)
 
-    belt.unshift(belt.step())
-    return belt
-  }
-
-  belt.max = opt.max || 4
-  belt.step = opt.step || Object
-  belt.expire = opt.expire || Object
-  belt.tid = setInterval( belt.rotate=rotate, opt.interval || 60000 )
-  if (belt.tid.unref) belt.tid.unref() // don't hold open the NodejS event loop
-  return rotate()
-
+  let tid = belt.tid = setInterval( belt.rotate, belt.interval )
+  if (tid.unref) tid.unref() // don't hold open the NodejS event loop
+  return belt.rotate()
 }
 
 export default hashbelt
